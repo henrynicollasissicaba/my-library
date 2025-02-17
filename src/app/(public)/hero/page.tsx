30f149750f321c53a82677e7b019f3b8c9ba7ec0
Henry Nicollas <henrynissicaba@gmail.com>
2025-02-17 14:28:28 -0300

    added favicon and metadata

--- src/app/(public)/hero/page.tsx
+++ src/app/(public)/hero/page.tsx
@@ -7,10 +7,10 @@ export default function Hero(){
             <Image src="/heroImg.svg" alt="hero image" width={500} height={500}/>
             <div className="flex flex-col justify-center items-center gap-3 w-full max-w-[30rem] md:h-[24rem] px-6 py-8 rounded-md shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">
                 <h1 className="font-bold text-xl">Seja bem vindo(a) ao <span className="text-primary-600">My Library</span>!</h1>
-                <p className="text-zinc-600">Aqui você pode cadastrar e gerenciar os livros que você possui, além de <span className="text-primary-600">criar anotações</span> sobre eles.</p>
+                <p className="text-zinc-600">Aqui você pode <span className="text-primary-600">cadastrar</span> e <span className="text-primary-600">gerenciar</span> os livros que você possui, além de <span className="text-primary-600">criar anotações</span> sobre eles.</p>
                 <p className="font-bold text-xl mt-3">Vamos começar?</p>
                 <div className="flex items-center gap-3 mt-5 flex-wrap">
-                    <Link 
+                    <Link
                         href="/register" 
                         className="w-full sm:max-w-fit text-center px-6 py-2 rounded-lg hover:text-white hover:bg-primary-500 transition-colors shadow-lg border"
                     >
--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer(){
     return(
         <p className="text-[.625rem] text-primary-600 text-center mt-auto mb-2 font-bold tracking-wide">
-            &copy; {new Date().getFullYear()} My Library | Desenvolvido por Henry Nicollas
+            &copy; {new Date().getFullYear()} My Library | Desenvolvido por 
+            <a 
+                href="https://www.linkedin.com/in/henry-nicollas-issicaba-neves-05a54024a/" 
+                target="_blank" 
+                className="border-b-2 border-primary-600"> Henry Nicollas</a>
         </p>
     )
 }
