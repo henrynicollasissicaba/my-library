18141881eb07517f4caa8b6e5fea786e0299a66e
Henry Nicollas <henrynissicaba@gmail.com>
2025-02-17 17:06:46 -0300

    removed unnecessary comments

--- src/schemas/bookSchema.ts
+++ src/schemas/bookSchema.ts
@@ -20,15 +20,4 @@ export const createBookSchema = z.object({
     status: z.enum(bookStatus)
 })
 
-// export const updateBookSchema = z.object({
-//     title: z.string().min(1, "O * TÍTULO * do livro é obrigatório.").max(255, "O título do livro deve ter no máximo 255 caracteres.").optional(),
-//     author: z.string().min(1, "O * AUTOR * do livro é obrigatório.").max(255, "O autor do livro deve ter no máximo 255 caracteres.").optional(),
-//     number_of_pages: z.string()
-//                     .min(1, "O * NÚMERO DE PÁGINAS * é obrigatório")
-//                     .transform((val) => Number(val))
-//                     .refine((val) => val > 0 && val < 4000, "O * NÚMERO DE PÁGINAS * deve estar entre 1 e 4000")
-//                     .optional(),
-//     category: z.enum(booksCategory).optional()
-// })
-
 export const updateBookSchema = createBookSchema.partial()
